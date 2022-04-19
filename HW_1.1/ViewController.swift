310c7406021ef1fa03fb60eedfab29dc0d518769
Yautrose <86356629+Yautrose@users.noreply.github.com>
2022-04-19 21:42:02 +0300

    Initial Commit

---
+++ HW_1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_1.1
+//
+//  Created by Alexey Artyushenko on 19.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
